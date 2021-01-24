bd05fbd3bd720a81232c1ab45d110e3cd092a4c2
Nick930826 <xianyou1993@qq.com>
2021-01-24 10:09:44 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>“喂你好，是尼克陈吗？我是叮咚买菜的送菜员，由于您电话打不通，特意在页面里跟您说一声，您菜到家了。</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const testFunction = () => {
+      console.log('滚')
+    }
+
+    return {
+      testFunction
+    }
+  }
+}
+</script>
+
+<style>
+  div {
+    color: yellowgreen;
+  }
+</style>
